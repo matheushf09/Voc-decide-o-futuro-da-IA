56a747efa61aa68d7bb420d2ccd17b977976def0
matheushf09 <124543319+matheushf09@users.noreply.github.com>
2024-07-31 08:03:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaalternativas = document.querySelector(".caixa-alternativas");
+const caixaresultados = document.querySelector(".caixa-resultados");
+const textoresultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativa: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }
+]
